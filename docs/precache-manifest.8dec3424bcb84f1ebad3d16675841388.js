a58676f3964ee662ce70e83f8db85604d109517c
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2020-03-29 23:55:22 +0100

    Update

---
+++ docs/precache-manifest.8dec3424bcb84f1ebad3d16675841388.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512c0fdee7d71ac8aeb948f6b1355cbd",
+    "url": "/react-with-apollo-graphql/index.html"
+  },
+  {
+    "revision": "3f077e036a2a1ed06a19",
+    "url": "/react-with-apollo-graphql/static/js/2.a8a4dfeb.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-with-apollo-graphql/static/js/2.a8a4dfeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdacfb490c63272e7565",
+    "url": "/react-with-apollo-graphql/static/js/main.23055582.chunk.js"
+  },
+  {
+    "revision": "7bc8b13f342c49765312",
+    "url": "/react-with-apollo-graphql/static/js/runtime-main.dbc7469d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a15545e8daad8bf7489f69b3e467c5a0.js"
+  "/react-with-apollo-graphql/precache-manifest.8dec3424bcb84f1ebad3d16675841388.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-with-apollo-graphql/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
